c8395a4b49131924961fe1b3e52163ed465c0ea0
Tsogoo07 <tsogbadrakh@callpro.mn>
2023-09-15 09:21:13 +0800

    project

--- degree/lib/main.dart
+++ degree/lib/main.dart
@@ -71,6 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Text('')
           ],
         ),
       ),
