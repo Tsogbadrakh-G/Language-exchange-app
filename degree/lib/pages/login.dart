d0691f31ade21306155343f830d732447c33d600
Tsogoo07 <135571617+Tsogoo07@users.noreply.github.com>
2023-10-22 22:11:26 +0800

    Update login.dart

--- degree/lib/pages/login.dart
+++ degree/lib/pages/login.dart
@@ -98,200 +98,214 @@ class _LogInState extends State<LogIn> {
           width: double.infinity,
           height: double.infinity,
           //  decoration: BoxDecoration(border: Border.all()),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
+          //  child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                        child: Text(
+                          "Сайн уу 👋",
+                          style: const TextStyle(
+                            fontFamily: "Outfit",
+                            fontSize: 37,
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xff000000),
+                            height: 37 / 37,
+                          ),
+                          textAlign: TextAlign.left,
+                        )),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                child: Form(
+                  key: _formkey,
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
+                      // Container(
+                      //   margin: EdgeInsets.symmetric(horizontal: 20),
+                      //   padding: EdgeInsets.fromLTRB(5, 0, 0, 10),
+                      //   width: double.infinity,
+                      //   child: Text('И-мэйл'),
+                      // ),
                       Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          width: double.infinity,
-                          padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                          child: Text(
-                            "Сайн уу 👋",
-                            style: const TextStyle(
-                              fontFamily: "Outfit",
-                              fontSize: 37,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xff000000),
-                              height: 37 / 37,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: TextFormField(
+                          focusNode: focusNode1,
+                          textAlignVertical: TextAlignVertical.center,
+                          controller: usermailcontroller,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter e-mail';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              //<-- SEE HERE
+                              borderSide:
+                                  BorderSide(width: 2, color: Colors.black38),
                             ),
-                            textAlign: TextAlign.left,
-                          )),
+                            focusedBorder: UnderlineInputBorder(
+                              //<-- SEE HERE
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Color(0xff2675EC),
+                              ),
+                            ),
+                            hintText: 'Имэйл-ээ оруулна уу',
+                            border: InputBorder.none,
+                            // prefixIcon: Icon(
+                            //   Icons.alternate_email,
+                            //   color: Color(0xFF7f30fe),
+                            // ),
+                          ),
+                        ),
+                      ),
                       SizedBox(
-                        height: 20,
+                        height: 30.0,
+                      ),
+                      // Container(
+                      //   margin: EdgeInsets.symmetric(horizontal: 20),
+                      //   padding: EdgeInsets.fromLTRB(5, 0, 0, 10),
+                      //   width: double.infinity,
+                      //   child: Text(
+                      //     'Нууц үг',
+                      //     style: TextStyle(color: Colors.black87),
+                      //   ),
+                      // ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: TextFormField(
+                          focusNode: focusNode2,
+                          textAlignVertical: TextAlignVertical.center,
+                          controller: userpasswordcontroller,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Нууц үгээ оруулна уу.';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              //<-- SEE HERE
+                              borderSide:
+                                  BorderSide(width: 2, color: Colors.black38),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              //<-- SEE HERE
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Color(0xff2675EC),
+                              ),
+                            ),
+                            border: InputBorder.none,
+                            hintText: 'Нууц үг',
+                            // prefixIcon: Icon(
+                            //   Icons.password,
+                            //   color: Color(0xFF7f30fe),
+                            // ),
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () => Get.to(ForgotPassword()),
+                            child: Container(
+                              alignment: Alignment.bottomRight,
+                              child: Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      Divider(),
+                      // const Spacer(),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        width: double.infinity,
+                        padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              // boxShadow: [
+                              //   BoxShadow(
+                              //     color: Colors.black38, // Shadow color
+                              //     offset: Offset(2, 2), // Shadow position (x, y)
+                              //     blurRadius: 4, // Spread of the shadow
+                              //     spreadRadius: 0, // How much the shadow should expand
+                              //   ),
+                              // ],
+                              ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formkey.currentState!.validate()) {
+                                setState(() {
+                                  email = usermailcontroller.text;
+                                  password = userpasswordcontroller.text;
+                                  //log('email: $email, pass: $password');
+                                });
+                              }
+                              userLogin();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              backgroundColor: Color(0xff2675EC),
+                            ),
+                            child: const Text(
+                              'Нэвтрэх',
+                              style: TextStyle(
+                                  decoration: TextDecoration.none,
+                                  color: Colors.white,
+                                  fontSize: 17),
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  width: double.infinity,
-                  child: Form(
-                    key: _formkey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          padding: EdgeInsets.fromLTRB(5, 0, 0, 10),
-                          width: double.infinity,
-                          child: Text('И-мэйл'),
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(width: 1.0, color: Colors.black38),
-                              // border: Border(
-                              //   bottom: BorderSide(color: Colors.black, width: 1),
-                              // ),
-                              borderRadius: BorderRadius.circular(5)),
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: TextFormField(
-                            focusNode: focusNode1,
-                            textAlignVertical: TextAlignVertical.center,
-                            controller: usermailcontroller,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter e-mail';
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                              hintText: 'Имэйл-ээ оруулна уу',
-                              border: InputBorder.none,
-                              // prefixIcon: Icon(
-                              //   Icons.alternate_email,
-                              //   color: Color(0xFF7f30fe),
-                              // ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30.0,
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          padding: EdgeInsets.fromLTRB(5, 0, 0, 10),
-                          width: double.infinity,
-                          child: Text(
-                            'Нууц үг',
-                            style: TextStyle(color: Colors.black87),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(width: 1.0, color: Colors.black38),
-                              borderRadius: BorderRadius.circular(5)),
-                          child: TextFormField(
-                            focusNode: focusNode2,
-                            textAlignVertical: TextAlignVertical.center,
-                            controller: userpasswordcontroller,
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Нууц үгээ оруулна уу.';
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'нууц үг',
-                              // prefixIcon: Icon(
-                              //   Icons.password,
-                              //   color: Color(0xFF7f30fe),
-                              // ),
-                            ),
-                            obscureText: true,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            GestureDetector(
-                              onTap: () => Get.to(ForgotPassword()),
-                              child: Container(
-                                alignment: Alignment.bottomRight,
-                                child: Text(
-                                  "Forgot Password?",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 14.0,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 50.0,
-                        ),
-                        Divider(),
-                        Container(
-                          margin: EdgeInsets.symmetric(horizontal: 20),
-                          width: double.infinity,
-                          padding: EdgeInsets.fromLTRB(0, 20, 0, 10),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                // boxShadow: [
-                                //   BoxShadow(
-                                //     color: Colors.black38, // Shadow color
-                                //     offset: Offset(2, 2), // Shadow position (x, y)
-                                //     blurRadius: 4, // Spread of the shadow
-                                //     spreadRadius: 0, // How much the shadow should expand
-                                //   ),
-                                // ],
-                                ),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                if (_formkey.currentState!.validate()) {
-                                  setState(() {
-                                    email = usermailcontroller.text;
-                                    password = userpasswordcontroller.text;
-                                    //log('email: $email, pass: $password');
-                                  });
-                                }
-                                userLogin();
-                              },
-                              style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30)),
-                                padding: EdgeInsets.symmetric(vertical: 15),
-                                backgroundColor: Color(0xff2675EC),
-                              ),
-                              child: const Text(
-                                'Нэвтрэх',
-                                style: TextStyle(
-                                    decoration: TextDecoration.none,
-                                    color: Colors.white,
-                                    fontSize: 17),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
+          // ),
         ),
       ),
     );
